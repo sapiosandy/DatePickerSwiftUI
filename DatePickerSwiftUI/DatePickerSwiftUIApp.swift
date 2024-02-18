00a2664d19114bd88723613156fc98724b372e4d
Sandy Gomez <sgomez1029@gmail.com>
2024-02-17 18:29:44 -0800

    Initial Commit

---
+++ DatePickerSwiftUI/DatePickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerSwiftUIApp.swift
+//  DatePickerSwiftUI
+//
+//  Created by Sandra Gomez on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
